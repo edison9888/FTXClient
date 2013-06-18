ef338b797fdf9b5ebfaa47d950df5f2eadb828f6
meekr <perry.lei@gmail.com>
2013-06-18 13:57:12 +0800

    Initial Commit

---
+++ FTXClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FTXClient
+//
+//  Created by Lei Perry on 6/18/13.
+//  Copyright (c) 2013 BitRice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
