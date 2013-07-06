5efafbcb30e100c27b086c1ad9da30f7894ed13e
meekr <perry.lei@gmail.com>
2013-07-06 23:53:13 +0800

    内容页面评论删除的按钮需要改样式

--- FTXClient/Views/Components/CommentTableViewCell.m
+++ FTXClient/Views/Components/CommentTableViewCell.m
@@ -6,8 +6,10 @@
 //  Copyright (c) 2013 BitRice. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
 #import "CommentTableViewCell.h"
 #import "UIColor+FTX.h"
+#import "UIImage+FTX.h"
 #import "UIImageView+AFNetworking.h"
 
 @interface CommentTableViewCell ()
@@ -64,6 +66,44 @@
 + (CGFloat)heightForCellWithReview:(Review *)review {
     CGSize size = [[NSString stringWithFormat:@"%@: %@", review.reviewer.name, review.content] sizeWithFont:[UIFont systemFontOfSize:13] constrainedToSize:CGSizeMake(249, CGFLOAT_MAX) lineBreakMode:NSLineBreakByWordWrapping];
     return fmaxf(44, size.height + 14);
+}
+
+- (void)willTransitionToState:(UITableViewCellStateMask)state {
+    [super willTransitionToState:state];
+    
+    if ((state & UITableViewCellStateShowingDeleteConfirmationMask) == UITableViewCellStateShowingDeleteConfirmationMask) {
+        for (UIView *subview in self.subviews) {
+            if ([NSStringFromClass([subview class]) isEqualToString:@"UITableViewCellDeleteConfirmationControl"]) {
+                UIView *deleteButtonView = (UIView *)[subview.subviews objectAtIndex:0];
+                
+                UIView *overlay = [[UIView alloc] initWithFrame:deleteButtonView.bounds];
+                overlay.backgroundColor = self.superview.backgroundColor;
+                [deleteButtonView addSubview:overlay];
+                
+                CGRect rect = CGRectInset(deleteButtonView.bounds, 4, 2);
+                rect.origin.x += 4;
+                UIImageView *image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"cell-delete-button"]];
+                image.frame = rect;
+                
+                UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+                button.frame = rect;
+                [button setBackgroundImage:[UIImage imageNamed:@"cell-delete-button"] forState:UIControlStateNormal];
+                [deleteButtonView addSubview:button];
+            }
+        }
+    }
+    else if ((state & UITableViewCellStateShowingDeleteConfirmationMask) == UITableViewCellStateDefaultMask) {
+        for (UIView *subview in self.subviews) {
+            if ([NSStringFromClass([subview class]) isEqualToString:@"UITableViewCellDeleteConfirmationControl"]) {
+                UIView *deleteButtonView = (UIView *)[subview.subviews objectAtIndex:0];
+                for (int i=0; i<deleteButtonView.subviews.count; i++) {
+                    UIButton *button = (UIButton *)deleteButtonView.subviews[i];
+                    if ([button respondsToSelector:@selector(setBackgroundImage:forState:)])
+                        [button setBackgroundImage:[UIImage imageNamed:@"cell-delete-button-selected"] forState:UIControlStateNormal];
+                }
+            }
+        }
+    }
 }
 
 @end
