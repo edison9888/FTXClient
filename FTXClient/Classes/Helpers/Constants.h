7b09f9f8917626b5a5404f8afe5d048067409186
meekr <perry.lei@gmail.com>
2013-07-22 10:33:01 +0800

    apply ftx umeng key

--- FTXClient/Classes/Helpers/Constants.h
+++ FTXClient/Classes/Helpers/Constants.h
@@ -1,5 +1,5 @@
 #define kAppStoreId @"502372808"
-#define kUMengAppKey @"51c003b256240b556b0c7b80"
+#define kUMengAppKey @"51ca575656240b81a504f65e"    //@"51c003b256240b556b0c7b80"
 
 #define kAccountChangeNotification @"kAccountChangeNotification"
 #define kCategoryIndexChangeNotification @"kCategoryIndexChangeNotification"
